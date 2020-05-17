49c51c958fffa5d3bd13510a185c8ac76a501609
wanjiaXG <1051249625@qq.com>
2020-05-18 02:19:33 +0800

    add pp-server

---
+++ pp-server/Controllers/PPController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PerformanceCalculator.Simulate;
+
+namespace pp_server.Controllers
+{
+    [ApiController]
+    public class PPController : ControllerBase
+    {
+        private readonly ILogger<PPController> _logger;
+
+        public PPController(ILogger<PPController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/")]
+        public IEnumerable<PPModel> index(int b, int m, string mods)
+        {
+            OsuSimulateCommand command = new OsuSimulateCommand();
+
+            _logger.LogInformation("???????????????",null);
+
+            List<PPModel> p = new List<PPModel>();
+            p.Add(new PPModel() { BeatmapId = b });
+            return p;
+
+        }
+    }
+}
---
+++ pp-server/PPModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pp_server
+{
+    public class PPModel
+    {
+        public int BeatmapId { set; get; }
+        public double Accuracy { set; get; }
+        public int Combo { set; get; }
+        public int Great { set; get; }
+        public int Good { set; get; }
+        public int Meh { set; get; }
+        public int Miss { set; get; }
+        public string[] Mods { set; get; }
+        public double Aim { set; get; }
+        public double Speed { set; get; }
+        public double Acc { set; get; }
+        public double OD { set; get; }
+        public double AR { set; get; }
+        public int MaxCombo { set; get; }
+        public double PP { set; get; }
+    }
+}
